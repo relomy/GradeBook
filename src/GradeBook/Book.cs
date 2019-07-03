a5eb261ce0e45320b868f4bc3ba6c2ee0151419f
Adam Lewandowski <adam.p.lewandowski@nasa.gov>
2019-07-02 21:33:52 -0400

    added 'powerful' switch statement for calculating Letter grade

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -11,6 +11,30 @@ namespace GradeBook
             Name = name;
         }
 
+        public void AddLetterGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                case 'F':
+                    AddGrade(50);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
         public void AddGrade(double grade)
         {
             if (grade >= 0 && grade <= 100) 
@@ -30,13 +54,21 @@ namespace GradeBook
             result.Low = double.MaxValue;
             result.High = double.MinValue;
 
-            foreach(var grade in grades)
+            foreach (var grade in grades)
             {
                 result.Low = Math.Min(grade, result.Low);
                 result.High = Math.Max(grade, result.High);
                 result.Average += grade;
             }
             result.Average /= grades.Count;
+
+            switch (result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+            }
 
             return result;
         }
